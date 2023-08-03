3490e65d41538e5f44fb0c77449ee0692031703e
mavias749 <141365828+mavias749@users.noreply.github.com>
2023-08-03 15:01:40 -0500

    Create Test.cs

---
+++ Test.cs
@@ -0,0 +1,15 @@
+int i = 1;
+
+switch (i)
+{
+    case 1:
+        Console.WriteLine("One");
+        break;
+    case 2:
+        Console.WriteLine("Two");
+        Console.WriteLine("Two");
+        break;
+    default:
+        Console.WriteLine("Other");
+        break;
+}
